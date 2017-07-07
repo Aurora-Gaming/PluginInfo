55fa9d7e9f4d89ce24b541abaa6ad7e8f1ddddd0
Ruby Rose <newy@aurora-terraria.org>
2017-07-07 10:17:54 +0300

    Use mono.options

--- PluginInfo/PluginInfo.cs
+++ PluginInfo/PluginInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-using NDesk.Options;
+using Mono.Options;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
